b98826e32c15f726de52176f6afbf97e75d7d0b9
DTyrsa <ADmitri42@gmail.com>
2016-02-20 22:40:56 +0300

    Yfxfk hfphf,

--- Этап 1.py
+++ Этап 1.py
@@ -4,4 +4,11 @@ Created on Sat Feb 20 21:35:05 2016
 
 @author: Dima
 """
-
+import pymongo
+client = pymongo.MongoClient('goto.reproducible.work')
+db = client['vk']
+data = db["users"]
+user = data.find({'interests': {'$exists': True}})
+#import json
+#print(json.dumps(user, indent=4, ensure_ascii=False)[:] + '...')
+print(user.count())
