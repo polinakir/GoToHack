c8e57940e0dc6cddba916b576105fbabb172eae4
funtrum <funtrum@Nicks-MacBook-Pro.local>
2016-02-20 22:35:38 +0300

    bDate to Age Added

--- nick.py
+++ nick.py
@@ -1,6 +1,7 @@
 # -*- coding: cp1251 -*-
 from pymongo import MongoClient
 import json
+from datetime import datetime
 
 c = MongoClient('goto.reproducible.work')
 print(c.database_names())
@@ -9,5 +10,24 @@ vk = c.vk
 print(vk.collection_names())
 
 users = vk.users
-query = users.find_one()
-print(query)
+
+q = users.find({},{"bdate":1})
+
+i=0
+while i < 15:
+	obj = q.next()
+	form_bdate = obj["bdate"]
+	b_date = datetime.strptime(form_bdate, '%d.%m.%Y')
+	print "Age : %d" % ((datetime.today() - b_date).days/365)
+	i += 1
+
+# Получение возраста 
+
+#uQuery = users.find({ }, { bdate: 1, id: 1 })
+
+#print(json.dumps(uQuery, sort_keys=True, indent=4))
+
+walls = vk.walls
+query = walls.find_one()
+
+#print(json.dumps(query, sort_keys=True, indent=4))
