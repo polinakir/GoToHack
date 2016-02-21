db560200d135981c1da7b7df17e182b78f628ae9
DTyrsa <ADmitri42@gmail.com>
2016-02-21 10:13:31 +0300

    j,yjdbk

--- Этап 1.py
+++ Этап 1.py
@@ -5,10 +5,23 @@ Created on Sat Feb 20 21:35:05 2016
 @author: Dima
 """
 import pymongo
+#import json
+
 client = pymongo.MongoClient('goto.reproducible.work')
 db = client['vk']
 data = db["users"]
-user = data.find({'interests': {'$exists': True}})
-#import json
+user = data.find({'interests': {'$exists': True},
+	'about': {'$exists': True},
+	'music': {'$exists': True},
+      'tv': {'$exists': True},#проверить
+	'books': {'$exists': True},
+	'games': {'$exists': True},
+	'movies': {'$exists': True},
+#	"personal": {
+#        "alcohol": {'$exists': True},
+#        "political": {'$exists': True},
+#        "smoking": {'$exists': True}
+#	}
+ })
 #print(json.dumps(user, indent=4, ensure_ascii=False)[:] + '...')
 print(user.count())
